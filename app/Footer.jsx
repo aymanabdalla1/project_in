513333aa452df5d58bb245d3a399efe7482350c6
aymanabdalla1 <107434617+aymanabdalla1@users.noreply.github.com>
2024-06-08 14:41:28 -0500

    fixed warnings/update bg

--- app/Footer.jsx
+++ app/Footer.jsx
@@ -11,16 +11,16 @@ const Footer = () => {
                     <div className="container-fluid d-flex justify-content-center w-75">
                         <Image src="/small_logo.png" height={60} width={60} alt="Project In Logo" />
                     </div>
-
+                    
                     <h5 className="text-center mt-4">Project In</h5>
                     <p className="text-center">All rights reserved</p>
 
-                    <div className="col-md-6 text-md-left mb-3 mb-md-0 mt-4 text-white">
+                    <div className="col-md-6 text-md-left mb-3 mb-md-0 mt-0 text-white">
                         <h5 className='text-center'>Contact Information</h5>
                         <p className='text-center'>Email: support@projectin.com</p>
                     </div>
                     
-                    <div className="col-md-6 text-md-right mt-4 text-white">
+                    <div className="col-md-6 text-md-right mt-0 text-white">
                         <h5 className='text-center'>Social Media Links</h5>
                         <ul className="list-inline text-center">
                             <li className="list-inline-item">
